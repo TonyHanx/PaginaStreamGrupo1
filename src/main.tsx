59d1ddea2be58564e96f4e2bbbac3b45547707f5
Arka <arkahans62@gmail.com>
2025-09-24 14:02:55 -0500

    Primer commit 09/24/2025

---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+// Importa los estilos CSS de Bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// Importa los scripts JavaScript de Bootstrap
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+  </StrictMode>,
+)
